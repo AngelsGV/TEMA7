fbd28c795323a011c45eea7b37da24b599bf1cc0
AngelsGV <angels.not99@gmail.com>
2024-02-05 21:37:09 +0100

    Initial commit

--- src/Resuelta7_2.java
+++ src/Resuelta7_2.java
@@ -1,4 +1,17 @@
 public class Resuelta7_2 {
+    public static void main(String[] ags){
+        CuentaCorriente1 c;
+        c = new CuentaCorriente1("12345678A","Pepe");
+        c.ingreso(1000);
+        c.egreso(300);
+        c.mostrarInformacion();
+        System.out.println("Puedo sacar 700 euros: " + c.egreso(700)); //quedan 0
+        System.out.println("Puedo sacar 500 euros: " + c.egreso(500));
+        //CUANDO SE REFERENCIA UN NUEVO OBJETO EL ANTERIOR QUEDA SIN REFERENCIA --> RECOLECTOR DE BASURA.
+        c = new CuentaCorriente1("986543F",2000);
+        c.mostrarInformacion();
+
+    }
 }
 class CuentaCorriente1{
     String dni;
@@ -14,4 +27,31 @@ class CuentaCorriente1{
         this.saldo = saldo;
         this.nombre = "Sin asignar";
     }
+    CuentaCorriente1(String dni, String nombre, double saldo){
+        this.dni = dni;
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+    boolean egreso(double cant){
+        //sacar dinero de la cuenta
+        boolean operacionPosible;
+        if (saldo >= cant ){
+            saldo -= cant;
+            operacionPosible = true;
+        }else{
+            //no hay saldo
+            System.out.println("No hay saldo suficiente.");
+            operacionPosible = false;
+        }
+        return (operacionPosible);
+    }
+    void ingreso(double cant){
+        saldo += cant;
+    }
+    void mostrarInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("DNI: " + dni);
+        System.out.println("Saldo: " + saldo);
+    }
 }
+
